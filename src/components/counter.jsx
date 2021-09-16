9acb32d0b1de871ff2c836660ff7e54dbd898db5
99mac99 <86948705+99mac99@users.noreply.github.com>
2021-09-16 12:28:54 +0200

    Dodawanie

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,4 @@
-import React, { Component } from 'react';
-
-
-
+import React, { Component }  from 'react';
 
 class  Counter extends Component {
     state = { 
@@ -9,35 +6,44 @@ class  Counter extends Component {
         tags:[]
     };
 
-    renderTags() {
-        if(this.state.tags.length === 0) return <p>There are no tags! </p>;
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
 
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    handleIncrement = () => {
+        this.setState({count: this.state.count + 1});
     }
 
     render() { 
         return (
         <div>
-            {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className = "btn btn-secondary btn-sm">Increment</button> */}
-            {this.state.tags.length === 0 && 'Please create a new tag!'}
-            {this.renderTags()}
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button onClick={this.handleIncrement} className = "btn btn-secondary btn-sm">Increment</button>
+
         </div>
         );
     };
 
-    // getBadgeClasses() {
-    //     let classes = "badge m-2 bg-";
-    //     classes += (this.state.count === 0) ? 'warning' : 'primary';
-    //     return classes;
-    // }
+    getBadgeClasses() {
+        let classes = "badge m-2 bg-";
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
+    };
 
-    // formatCount() {
-    //     const {count} = this.state;
-    //     return count === 0 ? 'Zero' : count;
-    // };
-
-
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count;
+    };
 };
  
 export default Counter; 
+
+
+    // renderTags() {
+    //     if(this.state.tags.length === 0) return <p>There are no tags! </p>;
+
+    //     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    // }
+    // {this.state.tags.length === 0 && 'Please create a new tag!'}
+    // {this.renderTags()}
