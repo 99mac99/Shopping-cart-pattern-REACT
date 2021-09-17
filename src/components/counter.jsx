995579ad4a4cf618313f3318397d8923dc0eede8
99mac99 <86948705+99mac99@users.noreply.github.com>
2021-09-17 13:29:23 +0200

    przesyłanie danych do komponentów

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,49 +1,48 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 
-class  Counter extends Component {
-    state = { 
-        count: 0,
-        tags:[]
-    };
+class Counter extends Component {
+	state = {
+		count: 0
+	};
+	handleIncrement = product => {
+		console.log(product);
+		this.setState({ count: this.state.count + 1 });
+	};
 
-    // constructor() {
-    //     super();
-    //     this.handleIncrement = this.handleIncrement.bind(this);
-    // }
-
-    handleIncrement = () => {
-        this.setState({count: this.state.count + 1});
-    }
-
-    render() { 
-        return (
-        <div>
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button onClick={this.handleIncrement} className = "btn btn-secondary btn-sm">Increment</button>
-
-        </div>
-        );
-    };
-
-    getBadgeClasses() {
-        let classes = "badge m-2 bg-";
-        classes += (this.state.count === 0) ? 'warning' : 'primary';
-        return classes;
-    };
-
-    formatCount() {
-        const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
-    };
-};
- 
-export default Counter; 
+	render() {
+		console.log('props', this.props)
+		return (
+			<div>
+				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+				<button
+					onClick={product => this.handleIncrement(product)}
+					className='btn btn-secondary btn-sm'
+				>
+					Increment
+				</button>
+			</div>
+		);
+	}
 
 
-    // renderTags() {
-    //     if(this.state.tags.length === 0) return <p>There are no tags! </p>;
+	getBadgeClasses() {
+		let classes = 'badge m-2 bg-';
+		classes += this.state.count === 0 ? 'warning' : 'primary';
+		return classes;
+	}
 
-    //     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-    // }
-    // {this.state.tags.length === 0 && 'Please create a new tag!'}
-    // {this.renderTags()}
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? 'Zero' : count;
+	}
+}
+
+export default Counter;
+
+// renderTags() {
+//     if(this.state.tags.length === 0) return <p>There are no tags! </p>;
+
+//     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+// }
+// {this.state.tags.length === 0 && 'Please create a new tag!'}
+// {this.renderTags()}
